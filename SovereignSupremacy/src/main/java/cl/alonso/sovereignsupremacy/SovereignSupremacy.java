57889285f06c62b68dee9b5e926587439b7770b7
Alonso333-RED <alonsitoar018@gmail.com>
2025-09-17 02:53:02 -0300

    Base Structure

---
+++ SovereignSupremacy/src/main/java/cl/alonso/sovereignsupremacy/SovereignSupremacy.java
@@ -0,0 +1,7 @@
+package cl.alonso.sovereignsupremacy;
+
+public class SovereignSupremacy {
+    public static void main(String[] args) {
+        System.out.println("Iniciando SovereignSupremacy...");
+    }
+}
